409217c9beab779d9576e31bfa76d1e6b760aedb
Max Scheiber <max@Maxs-MacBook-Pro.local>
2014-11-17 21:57:02 -0500

    add three users for fake data creation

---
+++ fake_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Users (username, password, email, full_name, affiliation, interests)
+VALUES ('PGCB', 'come_on_eileen', 'ted.guenin@gmail.com',
+    'Penn Guamanian Cooking Brigade', 'PAC', 'Going to Tanzania!');
+
+INSERT INTO Users (username, password, email, full_name, affiliation, interests)
+VALUES ('tfp', 'copabanana', 'tpeterson993@gmail.com', 'University Toppah',
+    'Chicago', 'Going to Chili\'s!');
+
+INSERT INTO Users (username, password, email, full_name, affiliation, interests)
+VALUES ('masonje', 'kalepa_ta_kala', 'masonje@wharton.upenn.edu', 'Jack Mason',
+    'Lambda Chi Alpha', 'Fraternities, ritualism');
