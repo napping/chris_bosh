356097a28856bfbe8cc106432f7d876ea3abd2d5
rigeljs <rigeljs@gmail.com>
2014-11-17 22:02:58 -0500

    add more users

--- fake_data.sql
+++ fake_data.sql
@@ -4,8 +4,15 @@ VALUES ('PGCB', 'come_on_eileen', 'ted.guenin@gmail.com',
 
 INSERT INTO Users (username, password, email, full_name, affiliation, interests)
 VALUES ('tfp', 'copabanana', 'tpeterson993@gmail.com', 'University Toppah',
-    'Chicago', 'Going to Chili\'s!');
+    'Chicago', 'Going to Chili''s!');
 
 INSERT INTO Users (username, password, email, full_name, affiliation, interests)
 VALUES ('masonje', 'kalepa_ta_kala', 'masonje@wharton.upenn.edu', 'Jack Mason',
     'Lambda Chi Alpha', 'Fraternities, ritualism');
+
+INSERT INTO Users (username, password, email, full_name, affiliation, interests)
+VALUES ('necordgren', 'Brucie2004', 'penncleeglub@mgail.com',  'N Eric Cordgren', 'University Choir', 'Bells!');
+
+INSERT INTO Users (username, password, email, full_name, affiliation, interests)
+VALUES ('pjmullz', 'stouffer4lyfe', 'mulletsonmullets@paul.paul', 'Paul Mullet', 'Art Club', '???');
+
