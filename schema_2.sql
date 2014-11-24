4f1bc6bd788b07714891496df40f5f8df22e8b27
Max Scheiber <max@Maxs-MacBook-Pro.local>
2014-11-24 02:01:15 -0500

    group is a reserved Oracle word...

--- schema_2.sql
+++ schema_2.sql
@@ -11,10 +11,10 @@ CREATE TABLE Users
 CREATE TABLE Media 
 (
     mid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     type            VARCHAR(20),
     privacy         VARCHAR(20),
-    CONSTRAINT pk PRIMARY KEY (mid, group, type),
+    CONSTRAINT pk PRIMARY KEY (mid, source, type),
     CONSTRAINT type_enum CHECK (type in ('Link', 'Photo', 'Video', 'Trip', 'Destination')),
     CONSTRAINT privacy_enum CHECK (privacy in ('private','sharedWithTripMembers','public'))
 );
@@ -22,25 +22,25 @@ CREATE TABLE Media
 CREATE TABLE Trip
 (
     tid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     name            VARCHAR(50) NOT NULL,
     packing_list    VARCHAR(256),
     expenses        VARCHAR(256),
     type            VARCHAR(20) DEFAULT 'Trip',
-    CONSTRAINT pk_trip PRIMARY KEY (tid, group),
+    CONSTRAINT pk_trip PRIMARY KEY (tid, source),
     CONSTRAINT type_trip CHECK (type = 'Trip'),
-    FOREIGN KEY (tid, group, type) REFERENCES Media(mid, group type)
+    FOREIGN KEY (tid, source, type) REFERENCES Media(mid, sourcetype)
 );
 
 CREATE TABLE Destination 
 (
     did             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     name            VARCHAR(20) NOT NULL,
     type            VARCHAR(20) DEFAULT 'Destination',
-    CONSTRAINT pk_destination PRIMARY KEY (did, group),
+    CONSTRAINT pk_destination PRIMARY KEY (did, source),
     CONSTRAINT type_destination CHECK (type = 'Destination'),
-    FOREIGN KEY (did, group, type) REFERENCES Media(mid, gropu, type)
+    FOREIGN KEY (did, source, type) REFERENCES Media(mid, gropu, type)
 );
 
 CREATE TABLE Notification 
@@ -59,43 +59,43 @@ CREATE TABLE Hashtag
 CREATE TABLE Album
 (
     aid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     name            VARCHAR(50),
     privacy         VARCHAR(20),
-    CONSTRAINT pk_album PRIMARY KEY (aid, group)
+    CONSTRAINT pk_album PRIMARY KEY (aid, source)
 );
 
 CREATE TABLE Link 
 (
     lid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     url             VARCHAR(512),
     type            VARCHAR(20) DEFAULT 'Link',
-    CONSTRAINT pk_link PRIMARY KEY (lid, group),
+    CONSTRAINT pk_link PRIMARY KEY (lid, source),
     CONSTRAINT type_link CHECK (type = 'Link'),
-    FOREIGN KEY (lid, group, type) REFERENCES Media(mid, group, type)
+    FOREIGN KEY (lid, source, type) REFERENCES Media(mid, source, type)
 );
 
 CREATE TABLE Photo 
 (
     pid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     url             VARCHAR(512),
     type            VARCHAR(20) DEFAULT 'Photo',
-    CONSTRAINT pk_photo PRIMARY KEY (pid, group),
+    CONSTRAINT pk_photo PRIMARY KEY (pid, source),
     CONSTRAINT type_photo CHECK (type = 'Photo'),
-    FOREIGN KEY (pid, group, type) REFERENCES Media(mid, group, type)
+    FOREIGN KEY (pid, source, type) REFERENCES Media(mid, source, type)
 );
 
 CREATE TABLE Video 
 (
     vid             INT NOT NULL,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     url             VARCHAR(512),
     type            VARCHAR(20) DEFAULT 'Video',
-    CONSTRAINT pk_video PRIMARY KEY (vid, group),
+    CONSTRAINT pk_video PRIMARY KEY (vid, source),
     CONSTRAINT type_video CHECK (type = 'Video'),
-    FOREIGN KEY (vid, group, type) REFERENCES Media(mid, group, type)
+    FOREIGN KEY (vid, source, type) REFERENCES Media(mid, source, type)
 );
 
 CREATE TABLE NotifiedOf 
@@ -119,10 +119,10 @@ CREATE TABLE GoesOn
 (
     username        VARCHAR(20),
     tid             INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_goeson PRIMARY KEY (username, tid, group),
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_goeson PRIMARY KEY (username, tid, source),
     FOREIGN KEY (username) REFERENCES Users(username),
-    FOREIGN KEY (tid, group) REFERENCES Trip(tid, group)    
+    FOREIGN KEY (tid, source) REFERENCES Trip(tid, source)    
 );
 
 CREATE TABLE PartOf 
@@ -130,10 +130,10 @@ CREATE TABLE PartOf
     tid             INT,
     did             INT,
     order_in_trip   INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_partof PRIMARY KEY (tid, order_in_trip, group),
-    FOREIGN KEY (tid, group) REFERENCES Trip(tid, group),
-    FOREIGN KEY (did, group) REFERENCES Destination(did, group)
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_partof PRIMARY KEY (tid, order_in_trip, source),
+    FOREIGN KEY (tid, source) REFERENCES Trip(tid, source),
+    FOREIGN KEY (did, source) REFERENCES Destination(did, source)
 );
 
 CREATE TABLE Describes 
@@ -141,10 +141,10 @@ CREATE TABLE Describes
     tag             VARCHAR(20),
     mid             INT,
     type            VARCHAR(20),
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_describes PRIMARY KEY (tag, mid, group, type),
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_describes PRIMARY KEY (tag, mid, source, type),
     FOREIGN KEY (tag) REFERENCES Hashtag(tag),
-    FOREIGN KEY (mid, group, type) REFERENCES Media(mid, group, type),
+    FOREIGN KEY (mid, source, type) REFERENCES Media(mid, source, type),
     CONSTRAINT type_enum_describes CHECK (type in ('Link', 'Photo', 'Video', 'Trip', 'Destination'))
 );
 
@@ -153,10 +153,10 @@ CREATE TABLE Owns
     username        VARCHAR(20),
     mid             INT,
     type            VARCHAR(20),
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     FOREIGN KEY (username) REFERENCES Users(username),
-    FOREIGN KEY (mid, group, type) REFERENCES Media(mid, group, type),
-    CONSTRAINT pk_owns PRIMARY KEY (mid, group, type),
+    FOREIGN KEY (mid, source, type) REFERENCES Media(mid, source, type),
+    CONSTRAINT pk_owns PRIMARY KEY (mid, source, type),
     CONSTRAINT type_enum_owns CHECK (type in ('Link', 'Photo', 'Video', 'Trip', 'Destination'))
 );
 
@@ -165,12 +165,12 @@ CREATE TABLE Rating
     username        VARCHAR(20),
     rating          INT,
     mid             INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
     type            VARCHAR(20),
     review          VARCHAR(4000),
     FOREIGN KEY (username) REFERENCES Users(username),
-    FOREIGN KEY (mid, group, type) REFERENCES Media(mid, group, type),
-    CONSTRAINT pk_rating PRIMARY KEY (username, mid, group, type),
+    FOREIGN KEY (mid, source, type) REFERENCES Media(mid, source, type),
+    CONSTRAINT pk_rating PRIMARY KEY (username, mid, source, type),
     CONSTRAINT type_enum_rating CHECK (type in ('Link', 'Photo', 'Video', 'Trip', 'Destination')),
     CONSTRAINT rating_enum CHECK (rating >= 1 AND rating <= 5)
 );
@@ -179,10 +179,10 @@ CREATE TABLE RequestTrip
 (
     username        VARCHAR(20),
     tid             INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_requesttrip PRIMARY KEY (username, tid, group,
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_requesttrip PRIMARY KEY (username, tid, source,
     FOREIGN KEY (username) REFERENCES Users(username),
-    FOREIGN KEY (tid, group) REFERENCES Trip(tid, group)
+    FOREIGN KEY (tid, source) REFERENCES Trip(tid, source)
 );
 
 CREATE TABLE InviteTrip
@@ -190,11 +190,11 @@ CREATE TABLE InviteTrip
     username1       VARCHAR(20),
     username2       VARCHAR(20),
     tid             INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_invitetrip PRIMARY KEY (username1, username2, tid, group),
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_invitetrip PRIMARY KEY (username1, username2, tid, source),
     FOREIGN KEY (username1) REFERENCES Users(username),
     FOREIGN KEY (username2) REFERENCES Users(username),
-    FOREIGN KEY (tid, group) REFERENCES Trip(tid, group)
+    FOREIGN KEY (tid, source) REFERENCES Trip(tid, source)
 );
 
 CREATE TABLE InAlbum
@@ -202,10 +202,10 @@ CREATE TABLE InAlbum
     aid             INT,
     mid             INT,
     type            VARCHAR(20),
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    FOREIGN KEY (aid, group) REFERENCES Album(aid, group),
-    FOREIGN KEY (mid, group, type) REFERENCES Media(mid, group, type),
-    CONSTRAINT pk_inalbum PRIMARY KEY (aid, mid, group),
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    FOREIGN KEY (aid, source) REFERENCES Album(aid, source),
+    FOREIGN KEY (mid, source, type) REFERENCES Media(mid, source, type),
+    CONSTRAINT pk_inalbum PRIMARY KEY (aid, mid, source),
     CONSTRAINT type_enum_inalbum CHECK (type in ('Photo', 'Video'))
 );
 
@@ -213,8 +213,8 @@ CREATE TABLE AlbumOfTrip
 (
     aid             INT,
     tid             INT,
-    group           VARCHAR(20) NOT NULL DEFAULT 'default',
-    CONSTRAINT pk_albumoftrip PRIMARY KEY (aid, tid, group),
-    FOREIGN KEY (aid, group) REFERENCES Album(aid, group),
-    FOREIGN KEY (tid, group) REFERENCES Trip(tid, group)
+    source          VARCHAR(20) NOT NULL DEFAULT 'default',
+    CONSTRAINT pk_albumoftrip PRIMARY KEY (aid, tid, source),
+    FOREIGN KEY (aid, source) REFERENCES Album(aid, source),
+    FOREIGN KEY (tid, source) REFERENCES Trip(tid, source)
 );
